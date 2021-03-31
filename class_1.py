1126a96eccf3417f74ab409e39eec3e9fb14b037
Idris Fagbemi <idrisniyi94@gmail.com>
2021-03-31 18:08:41 -0500

    added new file

---
+++ class_1.py
@@ -0,0 +1,8 @@
+#Enter first and last name
+names = input("Enter first and last name: ")
+print(names)
+print()
+#Enter your goals for this class
+print("What are your goals for this class - long term goals?")
+goals = input("What would you like to be...when all is said and done with the class?")
+print(goals)
